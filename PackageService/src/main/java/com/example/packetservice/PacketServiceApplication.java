bbef42db585cde36726d0395938c289024b8c391
Soberanis <jcsru13@gmail.com>
2023-04-03 19:49:53 -0600

    Add: Modelos del microservicio de paquetes y la configuración básica del servidor

---
+++ PackageService/src/main/java/com/example/packetservice/PacketServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.packetservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PacketServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PacketServiceApplication.class, args);
+    }
+
+}
---
+++ PackageService/src/test/java/com/example/packetservice/PacketServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.packetservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PacketServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
