fa0360a2449facba2fa7ffc787259572bde7e391
Soberanis <jcsru13@gmail.com>
2023-04-09 12:00:34 -0600

    Update: Configuración de eureka completa

--- CheckpointService/src/main/java/com/example/checkpointservice/CheckpointServiceApplication.java
+++ CheckpointService/src/main/java/com/example/checkpointservice/CheckpointServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.checkpointservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CheckpointServiceApplication {
 
     public static void main(String[] args) {
