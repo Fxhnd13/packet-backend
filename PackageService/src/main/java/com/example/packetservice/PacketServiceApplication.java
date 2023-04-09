a3ae43a8de6a6da6555aaab9b4d8acac70976a33
Soberanis <jcsru13@gmail.com>
2023-04-09 12:01:33 -0600

    Update: Configuración con eureka completa para el servicio de paquetes

--- PackageService/src/main/java/com/example/packetservice/PacketServiceApplication.java
+++ PackageService/src/main/java/com/example/packetservice/PacketServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.packetservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PacketServiceApplication {
 
     public static void main(String[] args) {
