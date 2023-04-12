04ee829b73759d9613377f5d234db820a52adc65
Soberanis <jcsru13@gmail.com>
2023-04-12 17:19:16 -0600

    Update: Ahora el endpoint 'is-authenticated' retorna el string con el rol del usuario logueado

--- AuthenticationService/src/main/java/com/example/authenticationservice/controllers/AuthController.java
+++ AuthenticationService/src/main/java/com/example/authenticationservice/controllers/AuthController.java
@@ -1,17 +1,30 @@
 package com.example.authenticationservice.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/v1/is-authenticated")
 public class AuthController {
 
     @GetMapping
-    public ResponseEntity<Boolean> isAuthenticated(){
-        return ResponseEntity.ok(true);
+    public ResponseEntity<String> isAuthenticated(){
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            List<String> roles = new ArrayList<>();
+
+            for (GrantedAuthority authority : authentication.getAuthorities()) {
+                roles.add(authority.getAuthority());
+            }
+
+            return ResponseEntity.ok(roles.get(0));
     }
 
 }
