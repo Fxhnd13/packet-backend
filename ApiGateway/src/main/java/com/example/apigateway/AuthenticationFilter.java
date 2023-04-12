c1d7218030e32b667fe31a2580a7883576ad8918
Soberanis <jcsru13@gmail.com>
2023-04-12 15:06:40 -0600

    Add: api gateway para redireccionar a los distintos microservicios

---
+++ ApiGateway/src/main/java/com/example/apigateway/AuthenticationFilter.java
@@ -0,0 +1,82 @@
+package com.example.apigateway;
+
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.GatewayFilterFactory;
+import org.springframework.cloud.gateway.filter.factory.RedirectToGatewayFilterFactory;
+import org.springframework.http.*;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.ObjectInputFilter;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
+
+    public AuthenticationFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public Config newConfig() {
+        return new Config();
+    }
+
+    @Override
+    public String name() {
+        return "AuthenticationFilter";
+    }
+
+    public static class Config {
+        // configuración del filtro aquí
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            final List<String> apiEndpoints = List.of(
+                    "/authentication-service/v1/auth/register",
+                    "/authentication-service/v1/auth/authenticate"
+            );
+            Predicate<ServerHttpRequest> isApiSecured = r -> apiEndpoints.stream()
+                    .noneMatch(uri -> r.getURI().getPath().contains(uri));
+
+            if(isApiSecured.test(request)){
+                if(!request.getHeaders().containsKey("Authorization")){
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return response.setComplete();
+                }
+
+                final String token = request.getHeaders().getOrEmpty("Authorization").get(0);
+
+                try{
+                    RestTemplate isAuthenticatedRequest = new RestTemplate();
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.set("Authorization", token);
+                    HttpEntity<String> entity = new HttpEntity<>(headers);
+
+                    ResponseEntity<Boolean> isAuthenticatedResponse = isAuthenticatedRequest.exchange("http://localhost:8086/v1/is-authenticated", HttpMethod.GET, entity, Boolean.class);
+                    System.out.println(isAuthenticatedResponse);
+                    if(isAuthenticatedResponse.getStatusCode() != HttpStatus.OK){
+                        ServerHttpResponse response = exchange.getResponse();
+                        response.setStatusCode(isAuthenticatedResponse.getStatusCode());
+                        return response.setComplete();
+                    }
+
+                }catch (Exception e){
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.setStatusCode(HttpStatus.BAD_REQUEST);
+                    return response.setComplete();
+                }
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+}
---
+++ ApiGateway/src/main/java/com/example/apigateway/GatewayConfig.java
@@ -0,0 +1,10 @@
+package com.example.apigateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class GatewayConfig {
+}
