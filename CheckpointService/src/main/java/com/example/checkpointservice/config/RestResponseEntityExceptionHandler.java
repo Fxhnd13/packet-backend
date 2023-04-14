3f0fae146e105af4db78d541b2f90e5515b1224e
Soberanis <jcsru13@gmail.com>
2023-04-14 14:52:36 -0600

    Add: Se implemento la anotación para validación de roles consumiendo el método para obtener los roles del usuario

--- CheckpointService/src/main/java/com/example/checkpointservice/config/RestResponseEntityExceptionHandler.java
+++ CheckpointService/src/main/java/com/example/checkpointservice/config/RestResponseEntityExceptionHandler.java
@@ -1,2 +1,21 @@
-package com.example.checkpointservice.config;public class RestResponseEntityExceptionHandler {
+package com.example.checkpointservice.config;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.nio.file.AccessDeniedException;
+
+@ControllerAdvice
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler({AccessDeniedException.class})
+    public ResponseEntity<String> handleAccessDeniedException(Exception ex, WebRequest request){
+        return new ResponseEntity<>("No tienes permiso para acceder al recurso solicitado", new HttpHeaders(), HttpStatus.FORBIDDEN);
+    }
+
 }
--- CheckpointService/src/main/java/com/example/checkpointservice/controllers/DemoController.java
+++ CheckpointService/src/main/java/com/example/checkpointservice/controllers/DemoController.java
@@ -1,5 +1,6 @@
 package com.example.checkpointservice.controllers;
 
+import com.example.basedomain.auth.RoleValidation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/v1")
 public class DemoController {
 
-    @GetMapping("/hello-world")
-    public ResponseEntity<String> helloWorld(){
-        return ResponseEntity.ok("Hola mundo desde servicio de puntos de control");
+    @GetMapping("/hello-admin")
+    @RoleValidation({"ADMINISTRADOR","OPERADOR"})
+    public ResponseEntity<String> helloAdmin(){
+        return ResponseEntity.ok("Hola mundo desde servicio de puntos de control con permisos de administrador y operador");
+    }
+
+    @GetMapping("/hello-client")
+    @RoleValidation({"CLIENTE"})
+    public ResponseEntity<String> helloClient(){
+        return ResponseEntity.ok("Hola mundo desde servicio de puntos de control con permisos de cliente");
     }
 }
