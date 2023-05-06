0b125998ee5999d54de9395e3fa2052dace2ddff
Soberanis <jcsru13@gmail.com>
2023-05-06 11:14:02 -0600

    Update: Se agregó la configuración de autenticación al servicio de paquetes, así como configuraciones de compilación y credenciales de conexión con base de datos.

--- PackageService/src/main/java/com/example/packetservice/PacketServiceApplication.java
+++ PackageService/src/main/java/com/example/packetservice/PacketServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.authconfigurations","com.example.packetservice"})
 @EnableDiscoveryClient
 public class PacketServiceApplication {
 
