b77241bd0ae84b9657e0b397ef3c1f43d7e3a944
Soberanis <jcsru13@gmail.com>
2023-05-14 18:04:51 -0600

    Delete: Limpieza de proyecto gateway, clases que quedaron obsoletas

--- AuthConfigurations/src/main/java/com/example/authconfigurations/AuthConfigurationsApplication.java
+++ AuthConfigurations/src/main/java/com/example/authconfigurations/AuthConfigurationsApplication.java
@@ -1,9 +1,7 @@
 package com.example.authconfigurations;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class AuthConfigurationsApplication {
 
 	public static void main(String[] args) {
